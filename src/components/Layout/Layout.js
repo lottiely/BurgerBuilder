6edb9e2c47307265dd3b7ff7619cad9d054f7c0c
lottiely <saialmario1@gmail.com>
2020-02-21 14:48:03 -0800

    Added side drawer for mobile.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 import classes from './Layout.module.css';
 
 const layout = ( props ) => (
     <Auxiliary>
         <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>
             { props.children }
         </main>
